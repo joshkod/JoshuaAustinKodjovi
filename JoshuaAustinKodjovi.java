ca040d4d654d257dd37ad00bb9ce7e59f1c2bd0f
joshkod <sba24256@student.cct.ie>
2024-10-27 18:10:07 +0100

    Update JoshuaAustinKodjovi.java
    Adding try buffered reader, writer, file out and the while loop

---
+++ JoshuaAustinKodjovi.java
@@ -0,0 +1,42 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+package com.mycompany.joshuaaustinkodjovi;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Calendar;
+
+public class JoshuaAustinKodjovi {
+   public JoshuaAustinKodjovi() {
+   }
+
+   public static void main(String[] args) {
+      String inputFileName = "C:\\\\Users\\\\10 Prestige\\\\Documents\\\\CCT_Dublin\\\\Diploma_in_Applied_Software_Development\\\\Programming&Mathematics_Fundamentals\\\\Assignments\\\\JoshuaAustinKodjovi\\\\src\\\\main\\\\java\\\\com\\\\mycompany\\\\joshuaaustinkodjovi\\\\customers.txt";
+
+      String outputFileName = "customerdiscount.txt";
+
+      try {
+         BufferedReader br = new BufferedReader(new FileReader(inputFileName));
+
+         try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(outputFileName));
+
+      String fullName;
+            try {
+               while((fullName = br.readLine()) != null) {
+                  double valueOfPurchase = Double.parseDouble(br.readLine());
+                  int classType = Integer.parseInt(br.readLine());
+                  int lastPurchase = Integer.parseInt(br.readLine());
+                  if (isValidClassType(classType)) {
+                     if (!isValidYear(lastPurchase)) {
+                        System.out.println("Error: Invalid year for " + fullName);
+                     } else {
+                        double finalValue = calculateFinalValue(valueOfPurchase, classType, lastPurchase);
+                        bw.write(fullName + "\n");
+                        Object[] var10002 = new Object[]{finalValue};
+                        bw.write(String.format("%.2f", var10002) + "\n");
+                     }
+                  }
+               }
